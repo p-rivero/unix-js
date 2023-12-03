cc46b672232324c91ca0cd3c62799ad100a93ff8
privero <65060696+p-rivero@users.noreply.github.com>
2023-12-03 20:17:16 +0100

    Update package.json

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-import { RootDirectory } from '@/filesystem/directories/root-directory'
-
-const dir = new RootDirectory({
-    type: 'directory',
-    internalName: 'home',
-    children: [
-        {
-            type: 'text-file',
-            internalName: 'README.md',
-            content: 'Hello world!'
-        }
-    ]
-})
-
-
-console.log(dir)
+export { Shell } from './shell/shell'
