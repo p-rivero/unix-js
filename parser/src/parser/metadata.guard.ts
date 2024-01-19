2234d90fc41c5a39bc44a8d67f4b15e747607c4e
privero <65060696+p-rivero@users.noreply.github.com>
2024-01-19 17:47:28 +0100

    Update parser package.json

--- parser/src/parser/metadata.guard.ts
+++ parser/src/parser/metadata.guard.ts
@@ -18,7 +18,19 @@ export function isFileMetadata(obj: unknown): obj is FileMetadata {
         (typeof typedObj["fileType"] === "undefined" ||
             typedObj["fileType"] === "text" ||
             typedObj["fileType"] === "binary" ||
-            typedObj["fileType"] === "device")
+            typedObj["fileType"] === "device") &&
+        (typeof typedObj["isStartupCommand"] === "undefined" ||
+            typedObj["isStartupCommand"] === false ||
+            typedObj["isStartupCommand"] === true) &&
+        (typeof typedObj["isStdin"] === "undefined" ||
+            typedObj["isStdin"] === false ||
+            typedObj["isStdin"] === true) &&
+        (typeof typedObj["isStdout"] === "undefined" ||
+            typedObj["isStdout"] === false ||
+            typedObj["isStdout"] === true) &&
+        (typeof typedObj["isStderr"] === "undefined" ||
+            typedObj["isStderr"] === false ||
+            typedObj["isStderr"] === true)
     )
 }
 
